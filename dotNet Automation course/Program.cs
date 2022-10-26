193bc9642f2590530daca19177e933750c2592f6
Jagoda Lebska <jagoda_lebska@epam.com>
2022-10-26 14:24:48 +0200

    Second Task

--- dotNet Automation course/Program.cs
+++ dotNet Automation course/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+class FirstTask
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Not enough numbers provided. Start the program again while prowiding a decimal number and a new base in the range 2-20");
+        }
+        else
+        {
+            int decimalValue;
+            bool isDecimalValue = Int32.TryParse(args[0], out decimalValue);
+            int newBase;
+            bool isNewBase = Int32.TryParse(args[1], out newBase);
+
+            if (isNewBase && isDecimalValue)
+            {
+
+                if (newBase < 2 || newBase > 20)
+                {
+                    Console.WriteLine("The base provided is out of range. Provide a number between 2 and 20.");
+                }
+                else
+                {
+                    Console.WriteLine($"Changing {decimalValue} to a {newBase} based number system");
+                    Console.WriteLine("----------------------------------------------------------");
+
+                    Console.WriteLine($"The {decimalValue} number in a {newBase} based number system is {ChangeBase(decimalValue, newBase)}.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Numbers provided are invalid. Try again");
+            }
+        }
+
+    }
+
+    static string ChangeBase(int decimalValue, int newBase)
+    {
+        string digits = "0123456789ABCDEFGIJKL";
+        int currentNumber = Math.Abs(decimalValue);
+        StringBuilder sb = new StringBuilder();
+
+        while(currentNumber != 0)
+        {
+            int remainder = currentNumber % newBase;
+            sb.Insert(0, digits[remainder]);
+            currentNumber = currentNumber / newBase;
+
+        }
+
+        return decimalValue >0 ? sb.ToString() : $"-{sb.ToString()}";
+        
+    }
+}
